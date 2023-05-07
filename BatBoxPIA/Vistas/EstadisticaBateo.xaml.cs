a1c75468d8b490495865722ef65367643589d017
cylon <cylon@LAPTOP-6HRP5VAK>
2023-05-07 11:46:42 -0600

    Logica de Bateo

--- BatBoxPIA/Vistas/EstadisticaBateo.xaml.cs
+++ BatBoxPIA/Vistas/EstadisticaBateo.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BatBoxPIA.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +17,55 @@ namespace BatBoxPIA.Vistas
             InitializeComponent();
         }
 
-     
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            
+                if (string.IsNullOrEmpty(
+                    txtNombreJugador.Text.Trim()) ||
+                    txtVecesAlBat.Text == "" ||
+                    txtHits.Text == "" ||
+                    txtDobletes.Text == "" ||
+                    txtTripletes.Text == "" ||
+                    txtHR.Text == "" ||
+                    txtBB.Text == "" ||
+                    txtHBP.Text == "" ||
+                    txtSAC.Text == "")
+
+                {
+                    DisplayAlert("Alert", "Es necesrio esten todos los valores", "Ok");
+                }
+                else
+                {
+                    
+                    double VecesAlBat = Convert.ToDouble(txtVecesAlBat.Text);
+                    double Hits = Convert.ToDouble(txtHits.Text);
+                    double Dobles = Convert.ToDouble(txtDobletes.Text);
+                    double Triples = Convert.ToDouble(txtTripletes.Text);
+                    double HR = Convert.ToDouble(txtHR.Text);
+                    double Bolas = Convert.ToDouble(txtBB.Text);
+                    double Golpe = Convert.ToDouble(txtHBP.Text);
+                    double SF = Convert.ToDouble(txtSAC.Text);
+
+                    double resultadoAVG = Hits / VecesAlBat;
+                    txtAVG.Text = Math.Round(resultadoAVG, 3).ToString();
+
+                    double resultadoOBP = (Hits + Bolas + Golpe) / (VecesAlBat + Bolas + Golpe + SF);
+                    txtOBP.Text = Math.Round(resultadoOBP, 3).ToString();
+
+                    double DobletesSlug = Dobles;
+                    double TripletesSlug = Triples * 2;
+                    double HRSlug = HR * 3;
+                    double TotalSlug = Hits + DobletesSlug + TripletesSlug + HRSlug;
+
+                    double resultadoSlug = TotalSlug / VecesAlBat;
+                    txtSlugging.Text = Math.Round(resultadoSlug, 3).ToString();
+
+                    double resultadoOPS = resultadoSlug + resultadoOBP;
+                    txtOPS.Text = Math.Round(resultadoOPS, 3).ToString();
+
+
+                }
+        }
     }
+
 }
