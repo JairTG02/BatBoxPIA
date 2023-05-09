b11ff41365122c0f86c19e24d48941c040cd9a1b
cylon <cylon@LAPTOP-6HRP5VAK>
2023-05-08 18:17:36 -0600

    Login

--- BatBoxPIA/Vistas/Login.xaml.cs
+++ BatBoxPIA/Vistas/Login.xaml.cs
@@ -20,6 +20,30 @@ namespace BatBoxPIA.Vistas
         private void btnIniciarSesion_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new Menu());
+
+            if (txtUsuario.Text == "JairTG")
+            {
+                if (txtPassword.Text == "1234")
+                {
+                    DisplayAlert("Success", "Bienvenido al sistema" + "" + txtUsuario.Text, "Continue");
+
+                }
+                else
+                {
+                    DisplayAlert("Alert", "Error, Contraseña incorrecta para el usuario asignado", "Ok");
+                }
+            }
+            else
+            {
+                DisplayAlert("Alert", "Error, el usuario no existe o no es correcto", "Ok");
+            }
+
+            if (txtUsuario.Text == "")
+            {
+                DisplayAlert("Alert", "Favor de ingresar datos", "Ok");
+
+            }
+           
         }
 
         private void btnRegresarLogin_Clicked(object sender, EventArgs e)
